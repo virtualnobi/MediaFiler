6cd630b70b89dc34f7da679c031a94953d150568
virtualnobi <virtualnobi@zoho.com>
2017-07-26 23:35:18 +0200

    added translations

--- locale/collect-messages.bat
+++ locale/collect-messages.bat
@@ -1,4 +1,4 @@
 echo off
 del /F locale\MediaFiler.pot.old
 ren locale\MediaFiler.pot MediaFiler.pot.old
-"c:\Program Files\Python\2.7\Tools\i18n\pygettext.py" -k N_ -o locale\MediaFiler.pot *.py UI\*.py MediaFiler\*.py
+"c:\Program Files\Python\2.7\Tools\i18n\pygettext.py" -k N_ -o locale\MediaFiler.pot *.py UI\*.py Model\*.py
